a3db6fbedd2cbb20544b450b5de09f7910dad521
Matt Jenkins <matt@majenko.co.uk>
2017-08-18 16:07:33 +0100

    Initial import

---
+++ T6963.cpp
@@ -0,0 +1,138 @@
+#include <T6963.h>
+
+void T6963::initializeDevice() {
+    pinMode(_wr, OUTPUT);
+    pinMode(_rd, OUTPUT);
+    pinMode(_cd, OUTPUT);
+    pinMode(_ce, OUTPUT);
+    pinMode(_d0, OUTPUT);
+    pinMode(_d1, OUTPUT);
+    pinMode(_d2, OUTPUT);
+    pinMode(_d3, OUTPUT);
+    pinMode(_d4, OUTPUT);
+    pinMode(_d5, OUTPUT);
+    pinMode(_d6, OUTPUT);
+    pinMode(_d7, OUTPUT);
+    digitalWrite(_wr, HIGH);
+    digitalWrite(_rd, HIGH);
+    digitalWrite(_cd, HIGH);
+    digitalWrite(_ce, LOW);
+    data(0);
+    data(0);
+    command(T6963_SET_GRAPHIC_HOME_ADDRESS);
+    data(256 / 8);
+    data(0);
+    command(T6963_SET_GRAPHIC_AREA);
+    data(0);
+    data(0);
+    command(T6963_SET_TEXT_HOME_ADDRESS);
+    data(256 / 8);
+    data(0);
+    command(T6963_SET_TEXT_AREA);
+    data(((32 / 2) - 1));
+    data(0x00);
+    command(T6963_SET_OFFSET_REGISTER);
+    command(T6963_DISPLAY_MODE | 0b1000);
+    command(T6963_MODE_SET);
+}
+
+void T6963::setPixel(int x, int y, color_t color) {
+    uint8_t row;
+    uint8_t col;
+    uint8_t pixel;
+    uint8_t mask;
+
+    if((x < 0) ||(x >= 256) || (y < 0) || (y >= 64))
+        return;
+
+    row = y;
+    col = x >> 3;
+    pixel = x & 0x07;
+    mask = 1<<(7 - pixel);
+    if (color) {
+        _buffer[col | (row * 32)] |= mask;
+    } else {
+        _buffer[col | (row * 32)] &= ~mask;
+    }
+
+    if (_buffered == 0) {
+        data(0);
+        data(0);
+        command(T6963_SET_ADDRESS_POINTER);
+        command(T6963_SET_DATA_AUTO_WRITE);
+
+        for (int i = 0; i < 2048; i++) {
+            data(_buffer[i]);
+        }
+
+        command(T6963_AUTO_RESET);
+    }
+}
+
+void T6963::data(uint8_t d) {
+    digitalWrite(_cd, LOW);
+    digitalWrite(_d0, d & 0x01);
+    digitalWrite(_d1, d & 0x02);
+    digitalWrite(_d2, d & 0x04);
+    digitalWrite(_d3, d & 0x08);
+    digitalWrite(_d4, d & 0x10);
+    digitalWrite(_d5, d & 0x20);
+    digitalWrite(_d6, d & 0x40);
+    digitalWrite(_d7, d & 0x80);
+    digitalWrite(_wr, LOW);
+    delayMicroseconds(1);
+    digitalWrite(_wr, HIGH);
+}
+
+void T6963::command(uint8_t d) {
+    digitalWrite(_cd, HIGH);
+    digitalWrite(_d0, d & 0x01);
+    digitalWrite(_d1, d & 0x02);
+    digitalWrite(_d2, d & 0x04);
+    digitalWrite(_d3, d & 0x08);
+    digitalWrite(_d4, d & 0x10);
+    digitalWrite(_d5, d & 0x20);
+    digitalWrite(_d6, d & 0x40);
+    digitalWrite(_d7, d & 0x80);
+    digitalWrite(_wr, LOW);
+    delayMicroseconds(1);
+    digitalWrite(_wr, HIGH);
+}
+
+void T6963::endBuffer() {
+    _buffered--;
+    if (_buffered <= 0) {
+        _buffered = 0;
+        data(0);
+        data(0);
+        command(T6963_SET_ADDRESS_POINTER);
+        command(T6963_SET_DATA_AUTO_WRITE);
+
+        for (int i = 0; i < 2048; i++) {
+            data(_buffer[i]);
+        }
+
+        command(T6963_AUTO_RESET);
+    }
+}
+
+void T6963::startBuffer() {
+    _buffered++;
+}
+
+void T6963::fillScreen(color_t c) {
+    memset(_buffer, c ? 255 : 0, 2048);
+    if (_buffered == 0) {
+        data(0);
+        data(0);
+        command(T6963_SET_ADDRESS_POINTER);
+        command(T6963_SET_DATA_AUTO_WRITE);
+
+        for (int i = 0; i < 2048; i++) {
+            data(_buffer[i]);
+        }
+
+        command(T6963_AUTO_RESET);
+    }
+}
+
---
+++ T6963.h
@@ -0,0 +1,85 @@
+#ifndef _T6963_H
+#define _T6963_H
+
+#include <DisplayCore.h>
+
+class T6963 : public DisplayCore {
+    private:
+    public:
+        uint8_t _buffer[2048];
+
+        uint8_t _rd;
+        uint8_t _wr;
+        uint8_t _cd;
+        uint8_t _ce;
+
+        uint8_t _d0;
+        uint8_t _d1;
+        uint8_t _d2;
+        uint8_t _d3;
+        uint8_t _d4;
+        uint8_t _d5;
+        uint8_t _d6;
+        uint8_t _d7;
+
+        int _buffered;
+
+        static const uint8_t T6963_CURSOR_PATTERN_SELECT = 0xA0;
+        static const uint8_t T6963_DISPLAY_MODE = 0x90;
+        static const uint8_t T6963_MODE_SET = 0x80;
+        static const uint8_t T6963_SET_CURSOR_POINTER = 0x21;
+        static const uint8_t T6963_SET_OFFSET_REGISTER = 0x22;
+        static const uint8_t T6963_SET_ADDRESS_POINTER = 0x24;
+        static const uint8_t T6963_SET_TEXT_HOME_ADDRESS = 0x40;
+        static const uint8_t T6963_SET_TEXT_AREA = 0x41;
+        static const uint8_t T6963_SET_GRAPHIC_HOME_ADDRESS = 0x42;
+        static const uint8_t T6963_SET_GRAPHIC_AREA = 0x43;
+        static const uint8_t T6963_SET_DATA_AUTO_WRITE = 0xB0;
+        static const uint8_t T6963_SET_DATA_AUTO_READ = 0xB1;
+        static const uint8_t T6963_AUTO_RESET = 0xB2;
+
+        static const uint8_t T6963_DATA_WRITE_AND_INCREMENT = 0xC0;
+        static const uint8_t T6963_DATA_READ_AND_INCREMENT = 0xC1;
+        static const uint8_t T6963_DATA_WRITE_AND_DECREMENT = 0xC2;
+        static const uint8_t T6963_DATA_READ_AND_DECREMENT = 0xC3;
+        static const uint8_t T6963_DATA_WRITE_AND_NONVARIALBE = 0xC4;
+        static const uint8_t T6963_DATA_READ_AND_NONVARIABLE = 0xC5;
+
+        static const uint8_t T6963_SCREEN_PEEK = 0xE0;
+        static const uint8_t T6963_SCREEN_COPY = 0xE8;
+
+        void data(uint8_t d);
+        void command(uint8_t c);
+
+
+
+    public:
+
+        T6963(
+            uint8_t rd, uint8_t wr, uint8_t cd, uint8_t ce,
+            uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+            uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7
+        ) : _rd(rd), _wr(wr), _cd(cd), _ce(ce),
+            _d0(d0), _d1(d1), _d2(d2), _d3(d3),
+            _d4(d4), _d5(d5), _d6(d6), _d7(d7),
+            _buffered(0) {
+            _width = 256;
+            _height = 64;
+        }
+
+        void initializeDevice();
+
+        void invertDisplay(boolean __attribute__((unused)) i) {}
+        void displayOn() {}
+        void displayOff() {}
+        void setRotation(int __attribute__((unused)) rotation) {}
+        void setPixel(int x, int y, color_t color);
+
+        void startBuffer();
+        void endBuffer();
+
+        void fillScreen(color_t c);
+
+};
+
+#endif
